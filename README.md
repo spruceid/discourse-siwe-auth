dd64ef10a8d07b01ce27a78f9ad2db6738083485
Jarek Radosz <jradosz@gmail.com>
2021-04-03 00:53:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plugin Name
+
+TODO
---
+++ plugin.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# name: discourse-plugin-name
+# about: TODO
+# version: 0.0.1
+# authors: Discourse
+# url: TODO
+
+enabled_site_setting :plugin_name_enabled
+
+after_initialize do
+end
