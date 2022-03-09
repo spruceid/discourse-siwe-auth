076dcef3ba98f443d96a4fe86c10bd5b583292bc
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2022-03-09 12:04:14 -0300

    Fix setting

--- app/controllers/discourse_siwe/auth_controller.rb
+++ app/controllers/discourse_siwe/auth_controller.rb
@@ -12,7 +12,7 @@ module DiscourseSiwe
       domain.slice!("#{Discourse.base_protocol}://")
       message = Siwe::Message.new(domain, eth_account, Discourse.base_url, "1", {
         issued_at: Time.now.utc.iso8601,
-        statement: SiteSettings.siwe_statement,
+        statement: SiteSetting.siwe_statement,
         nonce: Siwe::Util.generate_nonce,
         chain_id: params[:chain_id],
       })
