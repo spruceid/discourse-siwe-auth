fe762a274759373c85821f19da57487f30d102a1
0xjtgi <j@jtgi.me>
2023-02-22 08:57:34 +0900

    fix: rbsecp256k1 LoadError - rbsecp256k1 throws LoadError which is fixed in 5.1.1 - rbsecp256k1 5.1.1 in turn requires pkg-config 1.5.0

--- plugin.rb
+++ plugin.rb
@@ -12,12 +12,12 @@ register_asset 'stylesheets/discourse-siwe.scss'
   ../lib/omniauth/strategies/siwe.rb
 ].each { |path| load File.expand_path(path, __FILE__) }
 
-gem 'pkg-config', '1.4.7', require: false
+gem 'pkg-config', '1.5.0', require: false
 gem 'mkmfmf', '0.4', require: false
 gem 'keccak', '1.3.0', require: false
 gem 'zip', '2.0.2', require: false
 gem 'mini_portile2', '2.8.0', require: false
-gem 'rbsecp256k1', '5.1.0', require: false
+gem 'rbsecp256k1', '5.1.1', require: false
 gem 'konstructor', '1.0.2', require: false
 gem 'ffi', '1.15.5', require: false
 gem 'ffi-compiler', '1.0.1', require: false
